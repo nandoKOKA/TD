d31a54370123e31a0a343fa18b0f4514ce967f13
onlymr <a94122@alunos.uminho.pt>
2023-05-08 00:41:28 +0100

    detection class error solved

--- TD_APP/exemplo.py
+++ TD_APP/exemplo.py
@@ -17,8 +17,8 @@ font = cv2.FONT_HERSHEY_PLAIN
 #font = cv2.FONT_HERSHEY_COMPLEX
 Colors = np.random.uniform(0, 255, size=(len(classNames), 3))
 
-weightsPath = "frozen_inference_graph.pb"
-configPath = "ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+weightsPath = "C:\Users\onlymr\Desktop\TD\TD_APP\frozen_inference_graph.pb"
+configPath = "C:\Users\onlymr\Desktop\TD\TD_APP\ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
 net = cv2.dnn_DetectionModel(weightsPath,configPath)
 net.setInputSize(320,320)
 net.setInputScale(1.0/ 127.5)
--- TD_APP/main.py
+++ TD_APP/main.py
@@ -1,15 +1,15 @@
 import cv2
-
-img = cv2.imread('Perfil.png')
+import numpy as np 
+img = cv2.imread('C:\\Users\\onlymr\\Desktop\\TD\\TD_APP\\Perfil.png')
 
 classNames = []
-classFile = '/Users/tiagocarvalho/Documents/GitHub/TD/TD_APP/Labels.txt'
+classFile = "C:\\Users\\onlymr\\Desktop\\TD\\TD_APP\\Labels.txt"
 with open(classFile,'rt') as f:
     classNames = f.read().rstrip('\n').split('\n')
 #print(classNames)
 
-configPath = '/Users/tiagocarvalho/Downloads/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
-weightsPath = '/Users/tiagocarvalho/Downloads/frozen_inference_graph.pb'
+configPath = "C:\\Users\\onlymr\\Desktop\\TD\\TD_APP\\ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+weightsPath = "C:\\Users\\onlymr\\Desktop\\TD\\TD_APP\\frozen_inference_graph.pb"
 
 net = cv2.dnn_DetectionModel(weightsPath,configPath)
 net.setInputSize(320,320)
@@ -21,7 +21,7 @@ classIds, confs, bbox = net.detect(img,confThreshold=0.5)
 print(classIds,bbox)
 
 for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
-    cv2.rectangle(img,box,color=(0,255,0))
+    cv2.rectangle(img,box,color=(0,255,0), thickness=2)
     cv2.putText(img,classNames[classId-1],(box[0]+10,box[1]+30),
                  cv2.FONT_HERSHEY_COMPLEX,2,(0,255,0),2)
 
